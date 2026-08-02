0224cd98a869b3707ef1ddb67d3a21c800808878
Michael Weatherby <maw23456789@gmail.com>
2026-08-02 09:28:27 -0400

    add mannaquins, shulkers, copper golems

--- src/main/java/dev/smithed/radon/mixin/entity/AbstractVillagerMixin.java
+++ src/main/java/dev/smithed/radon/mixin/entity/AbstractVillagerMixin.java
@@ -10,8 +10,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.Objects;
-
 @Mixin(AbstractVillager.class)
 public abstract class AbstractVillagerMixin extends AgeableMobMixin implements ICustomNBTMixin {
 
