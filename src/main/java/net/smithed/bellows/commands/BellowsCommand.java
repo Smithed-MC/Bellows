011d097031040c054e2657b3860d95dd8f7d940d
Michael Weatherby <maw23456789@gmail.com>
2026-09-16 20:57:53 -0400

    increase version

--- src/main/java/net/smithed/bellows/commands/BellowsCommand.java
+++ src/main/java/net/smithed/bellows/commands/BellowsCommand.java
@@ -38,7 +38,7 @@ public class BellowsCommand {
      */
     public static int version(CommandContext<CommandSourceStack> context) {
         context.getSource().sendSuccess(() -> Component.literal("version = 1.0.0"), true);
-        return 10001;
+        return 10002;
     }
 
     /**
